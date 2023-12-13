83f0a3f8c6fef0002e5913a072d0bb4c03067384
mohit2978 <mohitrakheja007@gmail.com>
2023-12-13 22:49:04 +0530

    event binding

--- zerothApp/src/app/component/event-binding/event-binding.component.ts
+++ zerothApp/src/app/component/event-binding/event-binding.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-event-binding',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './event-binding.component.html',
   styleUrl: './event-binding.component.css'
 })
 export class EventBindingComponent {
-   buttonclick(){
+  getValue(event:KeyboardEvent){
+     let ip=event.target as HTMLInputElement;
+      console.log(ip);
+      console.log(ip.value);
+      
+  }
+  getValueDirect(ip:HTMLInputElement){
+     console.log(ip.value);
+     
+ }
+   buttonclick(event:any){
     alert("clicked the button");
+    this.count++;
+    this.styleObj.color='white'
+    this.styleObj.backgroundColor='black';
+    console.log(event);
+    
    }
+   count=0;
+   styleObj={
+    color:"green",
+    backgroundColor: "red"};
+   
 }
