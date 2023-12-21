a55f5cae23e3ed034c300730c10e4248485e0807
mohit2978 <mohitrakheja007@gmail.com>
2023-12-21 12:58:54 +0530

    ngswitch statement

--- DirectivesLearn/src/app/app.component.ts
+++ DirectivesLearn/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NgForComponent } from './components/ng-for/ng-for.component';
 import { SwitchCompComponent } from './components/switch-comp/switch-comp.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NgForComponent,SwitchCompComponent],
+  imports: [CommonModule, RouterOutlet,
+    NgForComponent,SwitchCompComponent,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- DirectivesLearn/src/app/components/switch-comp/switch-comp.component.ts
+++ DirectivesLearn/src/app/components/switch-comp/switch-comp.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-switch-comp',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './switch-comp.component.html',
   styleUrl: './switch-comp.component.css'
 })
 export class SwitchCompComponent {
-    n:number=2;
+    n:number=3;
+    number=1;
+    page=1;
 }
