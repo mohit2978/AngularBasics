f71f8e4cab742611e6aa75efb266776e59f88e4f
mohit <mohitrakheja007@gmail.com>
2024-02-06 16:08:00 +0530

    reactive form events

--- formsNoStandAlone/src/app/component/reactive-form/reactive-form.component.ts
+++ formsNoStandAlone/src/app/component/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormArray, FormBuilder, FormControl, FormGroup, RequiredValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,19 +7,63 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrl: './reactive-form.component.css',
   host: {ngSkipHydration: 'true'},
 })
-export class ReactiveFormComponent {
+export class ReactiveFormComponent implements OnInit {
+  constructor(private formBuilder:FormBuilder){
+
+  }
   userform=new FormGroup({
-    name:new FormControl(''),
+    name:new FormControl('',Validators.required),
     email:new FormControl(''),
     city:new FormControl(''),
     gender:new FormControl(''),
     about:new FormControl('')
   });
+  loginForm= this.formBuilder.group({
+    email:["test@gmail.com"],
+    pwd:["",Validators.required],
+    adress:this.formBuilder.group({
+      lane:["J-2978"],
+      sector:["Sector-49"],
+      city:[""]
+    }),
+    interests:this.formBuilder.array([])
+  })
+  ngOnInit(): void {
+    this.loginForm.get('email').valueChanges.subscribe(val=>{
+      console.log("value changes new value is "+val);
+      
+    });
+    this.loginForm.valueChanges.subscribe(val=>{
+      console.log("value 1 changes new value is ");
+      console.log(val);
+      
+    });
+  }
+  getInterest():FormArray{
+    return this.loginForm.get("interests") as FormArray
+  }
+  createInterest():FormGroup{
+    return this.formBuilder.group({
+      name:"",
+      timeInWeeks:['']
+    })
+  }
+   addInterest(){
+    this.getInterest().push(this.createInterest());
+   }
 
+   removeInterest(i:number){
+    this.getInterest().removeAt(i);
+   }
+  submitForm1(){
+    console.log(this.loginForm.value);
+    console.log("form submitted");
+    
+  }
   submitForm(){
     console.log(this.userform.value);
     console.log("form submitted");
     
-    
   }
+  
 }
